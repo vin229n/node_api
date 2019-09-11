6c5a761a93ad675f547eb401d90a80114c37aebc
Vinayak Patil <vinayak.p@BRILLIO.COM>
2019-09-11 13:16:05 +0530

    api

--- src/app.js
+++ src/app.js
@@ -20,36 +20,36 @@ app.use(express.json());
 app.use(cors())
 
 
-app.get('/weather',(req,res) => {
+app.get('api/weather',(req,res) => {
     if(!req.query.address) {
-        return res.send({
+        return res.status(500).send({
             error: 'You must provide search address'
         })
     }
 
     geocode(req.query.address,(error,{latitude, longitude, location}) =>{
         if(error){
-            return res.send(500,error)
+            return res.status(500).send(error)
         }
         else{
             forecast(latitude,longitude,(error,forecastdata) => {
                 if(error)
-                    return res.send(500,error)
+                    return res.status(500).send(error)
                 else{
-                    return res.send({location,forecastdata})
+                    return res.status(200).send({location,forecastdata})
                 }
             })
         }
     })
 })
 
-app.post('/register',(req,res) =>{
+app.post('api/register',(req,res) =>{
 	db.addUser(req.body)
 	
-	res.send(req.body)
+	res.status(200).send(req.body)
 })
 
-app.get('/users',(req,res) =>{
+app.get('api/users',(req,res) =>{
 	
 	db.listUsers((users)=>{
         console.log(users)
@@ -59,7 +59,7 @@ app.get('/users',(req,res) =>{
 })
 
 app.get('*',(req,res) =>{
-    res.send('<h1>404 page not found')
+    res.status(404).send('<h1>404 page not found')
 })
 
 app.listen(8000,()=>{
